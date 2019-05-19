3ae73a058469f763b3765564bc1bcb3a3e18f480
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-19 22:54:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,26 +1,23 @@
 class Triangle
 
-  attr_accessor :triangle
-
-  def initialize(side_a, side_b, side_c)
-    @triangle = [side_a, side_b, side_c].sort
-  end
-
-    def triangle
-      case @sides.uniq.count
-      when 1
-        return :equilateral
-      when 2
-        return :isosceles
-      when 3
-        return :scalene
-      raise TriangleError if notTriangle
-    end
+  attr_reader :triangle
+  def initialize(side1, side2, side3)
+    @triangle = [side1, side2, side3].sort
   end
 
   class TriangleError < StandardError
   end
 
+  def kind
+    raise TriangleError if sides[0] <= 0 or sides[0] + sides[1] <= sides[2]
 
-
+    case @triangle.uniq.count
+    when 1
+      return :equilateral
+    when 2
+      return :isosceles
+    when 3
+      return :scalene
+    end
+  end
 end
