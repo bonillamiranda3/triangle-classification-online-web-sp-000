9db8a08a1518e98aecd11103b75413e32b623cf7
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-19 22:52:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+
+  attr_accessor :triangle
+
+  def initialize(side_a, side_b, side_c)
+    @triangle = [side_a, side_b, side_c]
+  end 
+
+    def triangle
+      case @sides.uniq.count 
+      when 1
+        return :equilateral
+      when 2 
+        return :isosceles
+      when 3 
+        return :scalene
+      raise TriangleError if notTriangle
+    end   
+  end
+
+  class TriangleError < StandardError
+  end
+
+
+  
 end
